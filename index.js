bbf6d93164d7b46402d848da8fb8c4feaa6cc727
Neeoxx <mamannofficiel@gmail.com>
2022-02-22 13:25:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -155,65 +155,7 @@ if (message.content.startsWith('b!ban')) {
 if(message.content.startsWith('b!help')){
    message.channel.send(embed);
 }
-
-
-if (commande === 'lvl') {
-  if (bdd["statut-level"] == true) {
-      bdd["statut-level"] = false
-      Savebdd();
-      return message.channel.send('Vous venez d\'arreter le système de level !');
-  } else {
-      bdd["statut-level"] = true;
-      Savebdd();
-      return message.channel.send('Vous venez d\'alumer le système de level !');
-  }
-}
-
-if (bdd["statut-level"] == true) {
-  if (commande === 'level') {
-      if (!bdd["coins-utilisateurs"][message.member.id]) return message.channel.send(`Nous n'avez pas encore posté de message !`);
-      return message.channel.send(`Vous avez ${bdd["coins-utilisateurs"][message.member.id]} points !\nEt vous êtes au level n°${bdd["level-utilisateurs"][message.member.id]}`)
-  }
-  if (!bdd["coins-utilisateurs"][message.member.id]) {
-      bdd["coins-utilisateurs"][message.member.id] = Math.floor(Math.random() * (4 - 1) + 1);
-      bdd["level-utilisateurs"][message.member.id] = 0;
-      Savebdd();
-  } else {
-      let new_coins = bdd["coins-utilisateurs"][message.member.id] + Math.floor(Math.random() * (4 - 1) + 1);
-      if (bdd["coins-utilisateurs"][message.member.id] < 100 && new_coins >= 100) {
-          bdd["level-utilisateurs"][message.member.id] = 1;
-          bdd["coins-utilisateurs"][message.member.id] = new_coins;
-          Savebdd();
-          return message.channel.send(`Bravo ${message.author} tu es passé niveau 1 !`);
-      }
-      if (bdd["coins-utilisateurs"][message.member.id] < 250 && new_coins >= 250) {
-          bdd["level-utilisateurs"][message.member.id] = 2;
-          bdd["coins-utilisateurs"][message.member.id] = new_coins;
-          Savebdd();
-          return message.channel.send(`Bravo ${message.author} tu es passé niveau 2 !`);
-      }
-      if (bdd["coins-utilisateurs"][message.member.id] < 500 && new_coins > 500) {
-          bdd["level-utilisateurs"][message.member.id] = 3;
-          bdd["coins-utilisateurs"][message.member.id] = new_coins;
-          Savebdd();
-          return message.channel.send(`Bravo ${message.author} tu es passé niveau 3 !`);
-      }
-      if (bdd["coins-utilisateurs"][message.member.id] < 1000 && new_coins > 1000) {
-          bdd["level-utilisateurs"][message.member.id] = 4;
-          bdd["coins-utilisateurs"][message.member.id] = new_coins;
-          Savebdd();
-          return message.channel.send(`Bravo ${message.author} tu es passé niveau 4 !`);
-      }
-  }
-}
 })
-
-
-function Savebdd() {
-  fs.writeFile("./bdd.json", JSON.stringify(bdd, null, 4), (err) => {
-      if (err) message.channel.send("Une erreur est survenue");
-  });
-}
 
 //****LES GIFS****//
 Client.on('message', gotMessage);
@@ -274,8 +216,63 @@ Client.once('ready', () => {
         }
 
 
+        if (command === 'lvl') {
+          if (bdd["statut-level"] == true) {
+              bdd["statut-level"] = false
+              Savebdd();
+              return message.channel.send('Vous venez d\'arreter le système de level !');
+          } else {
+              bdd["statut-level"] = true;
+              Savebdd();
+              return message.channel.send('Vous venez d\'alumer le système de level !');
+          }
+        }
+        
+        if (bdd["statut-level"] == true) {
+          if (command === 'level') {
+              if (!bdd["coins-utilisateurs"][message.member.id]) return message.channel.send(`Nous n'avez pas encore posté de message !`);
+              return message.channel.send(`Vous avez ${bdd["coins-utilisateurs"][message.member.id]} points !\nEt vous êtes au level n°${bdd["level-utilisateurs"][message.member.id]}`)
+          }
+          if (!bdd["coins-utilisateurs"][message.member.id]) {
+              bdd["coins-utilisateurs"][message.member.id] = Math.floor(Math.random() * (4 - 1) + 1);
+              bdd["level-utilisateurs"][message.member.id] = 0;
+              Savebdd();
+          } else {
+              let new_coins = bdd["coins-utilisateurs"][message.member.id] + Math.floor(Math.random() * (4 - 1) + 1);
+              if (bdd["coins-utilisateurs"][message.member.id] < 100 && new_coins >= 100) {
+                  bdd["level-utilisateurs"][message.member.id] = 1;
+                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
+                  Savebdd();
+                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 1 !`);
+              }
+              if (bdd["coins-utilisateurs"][message.member.id] < 250 && new_coins >= 250) {
+                  bdd["level-utilisateurs"][message.member.id] = 2;
+                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
+                  Savebdd();
+                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 2 !`);
+              }
+              if (bdd["coins-utilisateurs"][message.member.id] < 500 && new_coins > 500) {
+                  bdd["level-utilisateurs"][message.member.id] = 3;
+                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
+                  Savebdd();
+                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 3 !`);
+              }
+              if (bdd["coins-utilisateurs"][message.member.id] < 1000 && new_coins > 1000) {
+                  bdd["level-utilisateurs"][message.member.id] = 4;
+                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
+                  Savebdd();
+                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 4 !`);
+              }
+          }
+        }
+        })
+      
+        function Savebdd() {
+          fs.writeFile("./bdd.json", JSON.stringify(bdd, null, 4), (err) => {
+              if (err) message.channel.send("Une erreur est survenue");
+          });
+        }
     });
-});
 
 
 Client.login(process.env.BOTTOKEN);
