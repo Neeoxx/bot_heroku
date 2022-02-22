7d7f6d8730b07083847b0a07c64cb7c8e40d0970
Neeoxx <mamannofficiel@gmail.com>
2022-02-22 13:42:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ const replies = [
     Client.user.setActivity("Les amoureux", { type: "WATCHING" });
   });
 
-  Client.on("message", message => {
+  Client.on("message", async message => {
+
     if (message.author.bot) return;
+
     if (message.channel.type == "dm") return;
 
     //b!ping
@@ -55,9 +57,62 @@ const replies = [
 
 
 
-    //****MUSIQUE****//
+    //****XP****//
+    if (message.content == prefix + "lvl") {
+      if (bdd["statut-level"] == true) {
+          bdd["statut-level"] = false
+          Savebdd();
+          return message.channel.send('Vous venez d\'arreter le système de level !');
+      } else {
+          bdd["statut-level"] = true;
+          Savebdd();
+          return message.channel.send('Vous venez d\'alumer le système de level !');
+      }
+  }
+  
+  if (bdd["statut-level"] == true) {
+      if (message.content == prefix + "level") {
+          if (!bdd["coins-utilisateurs"][message.member.id]) return message.channel.send(`Vous n'avez pas encore posté de message !`);
+          return message.channel.send(`Vous avez ${bdd["coins-utilisateurs"][message.member.id]} points !\nEt vous êtes au level n°${bdd["level-utilisateurs"][message.member.id]}`)
+      }
+      if (!bdd["coins-utilisateurs"][message.member.id]) {
+          bdd["coins-utilisateurs"][message.member.id] = Math.floor(Math.random() * (4 - 1) + 1);
+          bdd["level-utilisateurs"][message.member.id] = 0;
+          Savebdd();
+      } else {
+          let new_coins = bdd["coins-utilisateurs"][message.member.id] + Math.floor(Math.random() * (4 - 1) + 1);
+          if (bdd["coins-utilisateurs"][message.member.id] < 100 && new_coins >= 100) {
+              bdd["level-utilisateurs"][message.member.id] = 1;
+              bdd["coins-utilisateurs"][message.member.id] = new_coins;
+              Savebdd();
+              return message.channel.send(`Bravo ${message.author} tu es passé niveau 1 !`);
+          }
+          if (bdd["coins-utilisateurs"][message.member.id] < 250 && new_coins >= 250) {
+              bdd["level-utilisateurs"][message.member.id] = 2;
+              bdd["coins-utilisateurs"][message.member.id] = new_coins;
+              Savebdd();
+              return message.channel.send(`Bravo ${message.author} tu es passé niveau 2 !`);
+          }
+          if (bdd["coins-utilisateurs"][message.member.id] < 500 && new_coins > 500) {
+              bdd["level-utilisateurs"][message.member.id] = 3;
+              bdd["coins-utilisateurs"][message.member.id] = new_coins;
+              Savebdd();
+              return message.channel.send(`Bravo ${message.author} tu es passé niveau 3 !`);
+          }
+          if (bdd["coins-utilisateurs"][message.member.id] < 1000 && new_coins > 1000) {
+              bdd["level-utilisateurs"][message.member.id] = 4;
+              bdd["coins-utilisateurs"][message.member.id] = new_coins;
+              Savebdd();
+              return message.channel.send(`Bravo ${message.author} tu es passé niveau 4 !`);
+          }
+      }
+  }
 
-  //b!play
+  function Savebdd() {
+    fs.writeFile("./bdd.json", JSON.stringify(bdd, null, 4), (err) => {
+        if (err) message.channel.send("Une erreur est survenue.");
+    });
+}
 
 
 
@@ -214,64 +269,7 @@ Client.once('ready', () => {
         }else if (command ==='lewd'){
           Client.commands.get('lewd').execute(message, args, Discord);
         }
-
-
-        if (command === 'lvl') {
-          if (bdd["statut-level"] == true) {
-              bdd["statut-level"] = false
-              Savebdd();
-              return message.channel.send('Vous venez d\'arreter le système de level !');
-          } else {
-              bdd["statut-level"] = true;
-              Savebdd();
-              return message.channel.send('Vous venez d\'alumer le système de level !');
-          }
-        }
-        
-        if (bdd["statut-level"] == true) {
-          if (command === 'level') {
-              if (!bdd["coins-utilisateurs"][message.member.id]) return message.channel.send(`Nous n'avez pas encore posté de message !`);
-              return message.channel.send(`Vous avez ${bdd["coins-utilisateurs"][message.member.id]} points !\nEt vous êtes au level n°${bdd["level-utilisateurs"][message.member.id]}`)
-          }
-          if (!bdd["coins-utilisateurs"][message.member.id]) {
-              bdd["coins-utilisateurs"][message.member.id] = Math.floor(Math.random() * (4 - 1) + 1);
-              bdd["level-utilisateurs"][message.member.id] = 0;
-              Savebdd();
-          } else {
-              let new_coins = bdd["coins-utilisateurs"][message.member.id] + Math.floor(Math.random() * (4 - 1) + 1);
-              if (bdd["coins-utilisateurs"][message.member.id] < 100 && new_coins >= 100) {
-                  bdd["level-utilisateurs"][message.member.id] = 1;
-                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
-                  Savebdd();
-                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 1 !`);
-              }
-              if (bdd["coins-utilisateurs"][message.member.id] < 250 && new_coins >= 250) {
-                  bdd["level-utilisateurs"][message.member.id] = 2;
-                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
-                  Savebdd();
-                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 2 !`);
-              }
-              if (bdd["coins-utilisateurs"][message.member.id] < 500 && new_coins > 500) {
-                  bdd["level-utilisateurs"][message.member.id] = 3;
-                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
-                  Savebdd();
-                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 3 !`);
-              }
-              if (bdd["coins-utilisateurs"][message.member.id] < 1000 && new_coins > 1000) {
-                  bdd["level-utilisateurs"][message.member.id] = 4;
-                  bdd["coins-utilisateurs"][message.member.id] = new_coins;
-                  Savebdd();
-                  return message.channel.send(`Bravo ${message.author} tu es passé niveau 4 !`);
-              }
-          }
-        }
-        })
-      
-        function Savebdd() {
-          fs.writeFile("./bdd.json", JSON.stringify(bdd, null, 4), (err) => {
-              if (err) message.channel.send("Une erreur est survenue");
-          });
-        }
+        }) 
     });
 
 
