4970899ba67b57b17eee4213dc2e322ff13d394c
Neeoxx <mamannofficiel@gmail.com>
2022-02-26 13:55:51 +0100

    Update roh.js

--- commands/roh.js
+++ commands/roh.js
@@ -5,7 +5,7 @@ module.exports = {
         images = [
             "https://media4.giphy.com/media/iQHDtnUZ7gxI4/giphy.gif?cid=ecf05e47k4b7dvdu2mkn1nlf6em7lsjyakmtmc4p58905mox&rid=giphy.gif&ct=g",
             "https://media0.giphy.com/media/Y1Bl4MvP3fDq/giphy.gif?cid=ecf05e472n64rr3c08yw32o8tjb7k1y0nkoa47h4kdj6ame7&rid=giphy.gif&ct=g",
-            "https://media2.giphy.com/media/7AWcocwsCpxmg/giphy.gif?cid=ecf05e479o76w8fguofhf51y084mrzyoi7flmpptiidozvlq&rid=giphy.gif&ct=g"
+            "https://media2.giphy.com/media/7AWcocwsCpxmg/giphy.gif?cid=ecf05e479o76w8fguofhf51y084mrzyoi7flmpptiidozvlq&rid=giphy.gif&ct=g",
         ]
 
         personRoh = message.mentions.users.first()
